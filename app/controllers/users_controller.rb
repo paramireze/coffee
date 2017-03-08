244c21e3a2e9d3e39cf168578f563bd79a05bd64
paramireze <paramireze@gmail.com>
2017-03-08 11:52:49 -0600

    allow user forms to add/remove roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @purchases = @user.purchases.sorted
   end
 
   # GET /users/new
@@ -69,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :username)
+      params.require(:user).permit(:first_name, :last_name, :email, :username, role_ids: [])
     end
 end
