f5ff6903c3abe6688235e8eedf44ade9e1656214
paramireze <paramireze@gmail.com>
2017-03-15 20:55:30 -0500

    Changed root

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   get 'welcome/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'welcome#home'
+  root to: 'purchases#index'
 
   resources :items do
     member do
