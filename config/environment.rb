05a096fc2d9a0e6e215170315150978262dd1b08
paramireze <paramireze@gmail.com>
2017-08-28 16:06:34 -0500

    fixed date time display

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Time::DATE_FORMATS[:custom_datetime] = "%B %m, %Y %I:%M %p"
+Time::DATE_FORMATS[:custom_datetime] = "%B %d, %Y %I:%M %p"
 
